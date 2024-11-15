29aa1d837a985012d9e5e4a5cc9e13309de59e10
lengabson <est.gabriela.lenha@unimilitar.edu.co>
2024-11-15 13:50:10 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'UI_2024-2',
+  webDir: 'dist'
+};
+
+export default config;
